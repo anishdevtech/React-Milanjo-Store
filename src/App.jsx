c6c6a6c883fa9e4f768c415ecc353e88d8e49d9d
anishdevtech <anishdev02@gmail.com>
2023-12-29 16:26:58 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from './Components/Hero';
-import Service from "./Components/Service"
-import Testimonials from "./Components/Testimonials"
-import Pricing from "./Components/Pricing"
-import Footer from "./Components/Footer"
+import Service from './Components/Service';
+import Testimonials from './Components/Testimonials';
+import Pricing from './Components/Pricing';
+import Footer from './Components/Footer';
+import Cta from './Components/Cat';
+import Loading from './Components/Loading';
 
-
-import Cta from "./Components/Cat"
-// Your component or function where you want to use the Hero component
 const YourComponent = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous operation, like fetching data
+    const fetchData = async () => {
+      try {
+        // Simulate an API call with a delay
+        
+        // Once the data is fetched or operation is complete, set loading to false
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle error if needed and set loading to false
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
   return (
     <>
-     
-      <Hero />
-      <Cta />
-      <Service />
-      <Testimonials />
-      <Pricing />
-      <Footer />
       
-    </>
-  );
+        <Loading  />
+          <Hero />
+          <Cta />
+          <Service />
+          <Testimonials />
+          <Pricing />
+          <Footer />
+      </>
+      )
 };
 
 export default YourComponent;
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import NavBar from "./NavBar"
-const Hero = () => {
+
+
+export {Hero as default}
+
+const  Hero = () => {
+  
   return (
     <>
       <NavBar />
@@ -93,11 +98,8 @@ const Hero = () => {
             <div className="w-full px-4 lg:w-6/12">
               <div className="lg:ml-auto lg:text-right">
                 <div className="relative z-10 inline-block pt-11 lg:pt-0">
-                  <img
-                    src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
-                    alt="hero"
-                    className="max-w-full lg:ml-auto"
-                  />
+                  <HeroImg />
+           
                   <span className="absolute -bottom-8 -left-8 z-[-1]">
                     <svg
                       width="93"
@@ -142,8 +144,45 @@ const Hero = () => {
     </>
   );
 };
+import { motion } from "framer-motion";
 
-export default Hero;
+const HeroImg = () => {
+  const animationVariants = {
+    initial: {
+      opacity: 0,
+      x: "200%",
+      rotate: 0 ,
+    },
+    animate: {
+      opacity: 1,
+      x: "0%",
+      rotate: 0,
+      
+      transition: {
+        delay:5,
+        duration: 1,
+      },
+    },
+  };
+
+  return (
+    <motion.div 
+       style= {{ overflow:"hidden" }}
+      initial="initial"
+      animate="animate"
+      variants={animationVariants}
+    >
+      <img
+        src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
+        alt="hero"
+        className="max-w-full lg:ml-auto"
+        style={{ width: "100%", height: "auto" }} // Adjust dimensions as needed
+      />
+    </motion.div>
+  );
+};
+
+
 
 const SingleImage = ({ href, imgSrc }) => {
   return (
@@ -155,87 +194,3 @@ const SingleImage = ({ href, imgSrc }) => {
   );
 };
 
-const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <header className={`absolute left-0 top-0 z-20 flex w-full items-center`}>
-      <div className="container">
-        <div className="relative -mx-4 flex items-center justify-between">
-          <div className="w-60 max-w-full px-4">
-            <a href="/#" className="block w-full py-5">
-              <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo.svg"
-                alt="logo"
-                className="w-full dark:hidden"
-              />
-              <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-white.svg"
-                alt="logo"
-                className="w-full hidden dark:block"
-              />
-            </a>
-          </div>
-          <div className="flex w-full items-center justify-between px-4">
-            <div>
-              <button
-                onClick={() => setOpen(!open)}
-                id="navbarToggler"
-                className={` ${
-                  open && "navbarTogglerActive"
-                } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
-              >
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-              </button>
-              <nav
-                id="navbarCollapse"
-                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:shadow-none lg:dark:bg-transparent ${
-                  !open && "hidden"
-                } `}
-              >
-                <ul className="block lg:flex">
-                  <ListItem NavLink="/#">Home</ListItem>
-                  <ListItem NavLink="/#">Payment</ListItem>
-                  <ListItem NavLink="/#">About</ListItem>
-                  <ListItem NavLink="/#">Blog</ListItem>
-                </ul>
-              </nav>
-            </div>
-            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
-              <a
-                href="/#"
-                className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
-              >
-                Sign in
-              </a>
-
-              <a
-                href="/#"
-                className="rounded-lg bg-primary px-7 py-3 text-base font-medium text-white hover:bg-opacity-90"
-              >
-                Sign Up
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-const ListItem = ({ children, NavLink }) => {
-  return (
-    <>
-      <li>
-        <a
-          href={NavLink}
-          className="flex py-2 text-base font-medium text-dark hover:text-primary dark:text-white lg:ml-10 lg:inline-flex"
-        >
-          {children}
-        </a>
-      </li>
-    </>
-  );
-};
